e90ce5f79b319cb0c9278b3448c36f726c337c41
cheonjiyun <gunbam715@gmail.com>
2025-04-17 17:35:33 +0900

    펜 굵기 변경
    - popup window 로 굵기변경 창 띄움
    - seekbar 변경할 때 굵기 data 변경
    - 기존에 있던 선들은 굵기변경에 영향을 받지 않도록 strokes리스트로 각각 관리함

--- app/src/main/java/com/ssafy/drawdiary/Activity/DrawingActivity.kt
+++ app/src/main/java/com/ssafy/drawdiary/Activity/DrawingActivity.kt
@@ -1,24 +1,75 @@
 package com.ssafy.drawdiary.Activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ssafy.drawdiary.R
 import com.ssafy.drawdiary.databinding.ActivityDrawingBinding
+import com.ssafy.drawdiary.databinding.PopupPenBinding
+
+
+private const val TAG = "DrawingActivity_싸피"
 
 class DrawingActivity : AppCompatActivity() {
 
-    val binding : ActivityDrawingBinding by lazy {
+    val binding: ActivityDrawingBinding by lazy {
         ActivityDrawingBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_drawing)
+        setContentView(binding.root)
 
+        // 펜 굵기 설정 xml
+        val popupPenBinding = PopupPenBinding.inflate(layoutInflater, LinearLayout(this), false)
 
+        // 펜 굵기 data -> seekbar view에 반영
+        fun settingSeekBarByStoke() {
+            popupPenBinding.seekBarPenStroke.progress =
+                binding.customDrawView.currentStroke // 기존 굵기 반영
+        }
+
+        settingSeekBarByStoke()
+
+        // 값이 바뀔 때
+        popupPenBinding.seekBarPenStroke.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                binding.customDrawView.currentStroke = progress // 펜 굵기 바꾸기
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+
+        popupPenBinding.btnMinus.setOnClickListener {
+            binding.customDrawView.currentStroke = binding.customDrawView.currentStroke - 1
+            settingSeekBarByStoke()
+            Log.d(TAG, "onCreate: ${binding.customDrawView.currentStroke}")
+        }
+
+        popupPenBinding.btnPlus.setOnClickListener {
+            binding.customDrawView.currentStroke = binding.customDrawView.currentStroke + 1
+            settingSeekBarByStoke()
+        }
+
+        binding.pen.setOnClickListener {
+            // 팝업 윈도우 띄우기
+            val popupPenWindow = PopupWindow(
+                popupPenBinding.root,
+                popupPenBinding.root.layoutParams.width,
+                popupPenBinding.root.layoutParams.height,
+                true
+            )
+            popupPenWindow.showAsDropDown(binding.pen)
+        }
     }
+
+
 }
--- app/src/main/java/com/ssafy/drawdiary/CustomView/CustomDrawView.kt
+++ app/src/main/java/com/ssafy/drawdiary/CustomView/CustomDrawView.kt
@@ -8,20 +8,30 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
+private const val TAG = "CustomDrawView_싸피"
+
 class CustomDrawView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
+    data class Stroke(val path: Path, val paint: Paint)
 
-    private val path = Path()
-    private val paint = Paint().apply {
-        style = Paint.Style.STROKE
-    }
+    var currentStroke: Int = 10
+        set(value) {
+            field = when {
+                value < 1 -> 1
+                value > 100 -> 100
+                else -> value
+            }
+        }
 
+    private val strokes = mutableListOf<Stroke>() // 굵기를 바꾸었을 때 기존 굵기는 바뀌면 안되므로 선을 구분해서 list에 담음
+    private var currentPath: Path? = null
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawPath(path, paint)
+        for (stroke in strokes) {
+            canvas.drawPath(stroke.path, stroke.paint)
+        }
     }
-
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val x = event.x
@@ -29,11 +39,26 @@ class CustomDrawView(context: Context, attrs: AttributeSet?) : View(context, att
 
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                path.moveTo(x, y)
+
+                // 그릴위치
+                currentPath = Path().apply {
+                    moveTo(event.x, event.y)
+                }
+
+                // 굵기
+                val newPaint = Paint().apply {
+                    style = Paint.Style.STROKE
+                    strokeCap = Paint.Cap.ROUND
+
+                    strokeWidth = currentStroke.toFloat() // 굵기
+                }
+
+                // 선 정보에 저장
+                strokes.add(Stroke(currentPath!!, newPaint))
             }
 
             MotionEvent.ACTION_MOVE -> {
-                path.lineTo(x, y)
+                currentPath?.lineTo(x, y)
             }
 
             MotionEvent.ACTION_UP -> {
@@ -44,4 +69,5 @@ class CustomDrawView(context: Context, attrs: AttributeSet?) : View(context, att
 
         return true
     }
+
 }
