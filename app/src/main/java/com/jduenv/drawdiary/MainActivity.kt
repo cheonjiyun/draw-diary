31e02e72e8e87c08cd9a95aafd008f84d048ae62
천지윤 <gunbam715@gmail.com>
2025-06-08 14:42:13 +0900

    feat: 뒤로가기 버튼 두 번 누를 시 앱 종료 기능 구현

--- app/src/main/java/com/jduenv/drawdiary/MainActivity.kt
+++ app/src/main/java/com/jduenv/drawdiary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jduenv.drawdiary
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -72,6 +73,24 @@ class MainActivity : AppCompatActivity() {
         // 포그라운드로 돌아올 때마다 갱신
         val thumbs = loadThumbnails()
         thumbAdapter.updateData(thumbs)
+    }
+
+    // 마지막으로 뒤로 가기 버튼을 눌렀던 시간(밀리초)
+    private var lastBackPressedTime = 0L
+
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        // 2초(2000ms) 이내에 다시 눌렀으면
+        if (currentTime - lastBackPressedTime < 2000) {
+            super.onBackPressed()  // 앱 종료
+        } else {
+            lastBackPressedTime = currentTime
+            Toast.makeText(
+                this,
+                "뒤로 버튼을 한 번 더 누르면 앱이 종료됩니다",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 }
